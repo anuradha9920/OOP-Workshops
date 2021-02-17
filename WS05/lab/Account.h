a8d62d3d3091bfdf97aa84d53fbec0fbacc68f41
Anuradha <66209449+anuradha9920@users.noreply.github.com>
2021-02-17 18:59:54 +0530

    Update Account.h

--- WS05/lab/Account.h
+++ WS05/lab/Account.h
@@ -3,17 +3,27 @@
 #include <iostream>
 
 namespace sdds {
-   class Account {
+   class Account{
       int m_number;
       double m_balance; 
       void setEmpty();
-   public:
+    public:
       Account();
       Account(int number, double balance);
+      void setNew();
       std::ostream& display()const;
-
-
-
+      explicit operator int()const;
+      explicit operator double()const;
+      operator bool()const;
+      bool operator ~()const;
+      Account& operator =(int number);
+      Account& operator =(Account& acc);
+      Account& operator +=(double val);
+      Account& operator -=(double val);
+      Account& operator <<(Account& acc);
+      Account& operator >>(Account& acc);
+      friend  double operator +=(double& val,const Account& acc2);
+      friend double operator +(const Account& acc1,const Account& acc2);
    };
    
    
